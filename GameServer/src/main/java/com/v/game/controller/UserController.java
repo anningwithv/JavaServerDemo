c87d101fd91c9ae51b1aa5018cad56b40a3135ff
v <378294113@qq.com>
2024-08-19 20:38:57 +0800

    Redis

--- GameServer/src/main/java/com/v/game/controller/UserController.java
+++ GameServer/src/main/java/com/v/game/controller/UserController.java
@@ -7,6 +7,7 @@ import com.v.game.entity.User;
 import com.v.game.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +21,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+    
     /**
      * 用户注册
      */
